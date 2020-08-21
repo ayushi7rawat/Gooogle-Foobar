782b6ee5a59b605fba854cef7dac20dfb3c00c1e
Ayushi Rawat <ayushi7rawat@gmail.com>
2020-08-21 13:18:12 +0530

    level 1 sol

---
+++ Level 1/solution.py
@@ -0,0 +1,10 @@
+def solution(area):
+  import math
+  res = []
+
+  while (area > 0):
+    square_root = int(math.sqrt(area))
+    val = pow(square_root, 2)
+    res.append(val)
+    area -= val
+  return res
