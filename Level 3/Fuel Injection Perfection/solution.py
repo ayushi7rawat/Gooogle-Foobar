15b372f80811f69da30ef8ad6b583fdb46789f70
ayushi7rawat <ayushi7rawat@gmail.com>
2020-09-21 21:06:26 +0530

    Google foobar fuel injection

--- Level 3/Fuel Injection Perfection/solution.py
+++ Level 3/Fuel Injection Perfection/solution.py
@@ -2,4 +2,30 @@
 Problem Name: Google Foobar - Fuel Injection Perfection
 Author: Ayushi Rawat
 '''
+map = {}
+stack = []
+def solution(n):
+    n = int(n)
+    stack.append(n)
+    while len(stack) > 0:
+        n = stack[-1]
+        if map.get(n):
+            stack.pop()
+        elif n == 1:
+            map[n] = 0
+            stack.pop()
+        elif n % 2 == 0:
+            if map.get(n / 2) is not None:
+                map[n] = 1 + map[n / 2]
+                stack.pop()
+            else:
+                stack.append(n / 2)
+        else:
+            if map.get(n + 1) is not None and map.get(n - 1) is not None:
+                map[n] = 1 + min(map[n + 1], map[n - 1])
+                stack.pop()
+            else:
+                stack.append(n - 1)
+                stack.append(n + 1)
+    return map[n]
 
